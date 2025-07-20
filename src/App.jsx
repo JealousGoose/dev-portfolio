074fc6167588d00aeebd0bd07fd7f37702e5a327
unknown <ghimireshital001@gmail.com>
2025-07-20 22:32:38 +0545

    First code commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
+import Hero from "./sections/Hero";
+
 const App = ()=> {
     return (
-        <main>
-            <div>
-                <h1>
-                    Welcome to my 3d Portfolio
-                </h1>
-            </div>
-        </main>
+        <Hero/>
     )
 }
 
